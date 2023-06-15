16fe6570786ce528e785b46cc0a36d17f5be593d
Ömer Faruk ORUÇ <omerfarukoruc35@gmail.com>
2023-06-15 23:26:47 +0300

    Update boss_killer.py
    refactored whole code

--- boss_killer.py
+++ boss_killer.py
@@ -7,112 +7,104 @@ import win32api
 import win32con
 import DIKeys, hexKeyMap
 
+enter_raid_image = cv2.imread('enter_raid.png')
+enter_button_image = cv2.imread('enter_button.png')
+ashava_found_image = cv2.imread('ashava_found.png')
+ashava_dead_image = cv2.imread('ashava_dead.png')
 
-# Define the path to the image files and the image matching threshold
-ENTER_BUTTON_PATH = r'C:\Users\omer\Desktop\gpt-engineer-main\python\enter.png'
-ASHAVA_TEXT_PATH = r'C:\Users\omer\Desktop\gpt-engineer-main\python\ashava.png'
-ENTER_RAID_PATH = r'C:\Users\omer\Desktop\gpt-engineer-main\python\enter_raid.png'
-ENTER_RAID_PATH2 = r'C:\Users\omer\Desktop\gpt-engineer-main\python\enter_raid_2.png'
-EXIT_RAID_PATH = r'C:\Users\omer\Desktop\gpt-engineer-main\python\exit_raid.png'
+threshold = 0.9
+KEY1_INTERVAL = 7
+KEY2_INTERVAL = 18
+KEY3_INTERVAL = 10
+KEY4_INTERVAL = 8
+KEY5_INTERVAL = 23
 
-MATCH_THRESHOLD = 0.9
-MATCH_THRESHOLD_ASHAVA = 0.6
-MATCH_THRESHOLD_FIND_RAID = 0.25
-MATCH_THRESHOLD_EXIT_RAID = 0.4
+def clickEnterRaid():
 
-# Define the key press intervals in seconds
-KEY1_INTERVAL = 10
-KEY2_INTERVAL = 21
-KEY3_INTERVAL = 13
-KEY4_INTERVAL = 10
-KEY4_INTERVAL = 25
-
-# Load the image files for button and text matching
-enter_button_template = cv2.imread(ENTER_BUTTON_PATH, cv2.COLOR_BGR2GRAY)
-ashava_text_template = cv2.imread(ASHAVA_TEXT_PATH, cv2.COLOR_BGR2GRAY)
-enter_raid_template = cv2.imread(ENTER_RAID_PATH, cv2.COLOR_BGR2GRAY)
-enter_raid_template2 = cv2.imread(ENTER_RAID_PATH2, cv2.COLOR_BGR2GRAY)
-exit_raid_template = cv2.imread(EXIT_RAID_PATH, cv2.COLOR_BGR2GRAY)
-
-while True:
-
-    while True: 
-        # Find the Enter button
-        hwnd = win32gui.FindWindow(None, "Diablo Immortal")
-        if hwnd == 0:
-            print('Error: Diablo Immortal window not found.')
-            exit()
-
-        time.sleep(6)
-
-        rect = win32gui.GetWindowRect(hwnd)
-        x, y = rect[0], rect[1]
-        w, h = rect[2] - x, rect[3] - y
-
-        screen = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-        enter_raid_match = cv2.matchTemplate(screen, enter_raid_template, cv2.TM_CCOEFF_NORMED)
-        enter_raid_location = np.where(enter_raid_match >= MATCH_THRESHOLD_FIND_RAID)
-        if enter_raid_location[0].size > 0:
-            print('Enter button found.')
-            time.sleep(2)
-            enter_button_x = 200
-            enter_button_y = 300
-            win32gui.SetForegroundWindow(hwnd)
-            win32api.SetCursorPos((enter_button_x, enter_button_y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, enter_button_x, enter_button_y, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, enter_button_x, enter_button_y, 0, 0)
-            break
-        else:
-            print('Raid not found.')
-            time.sleep(1)
-
-    # Define the loop that searches for the button and text
     while True:
-        rect = win32gui.GetWindowRect(hwnd)
-        x, y = rect[0], rect[1]
-        w, h = rect[2] - x, rect[3] - y
+        print('Enter Raid button is detecting...')
+        time.sleep(1)
 
-        screen = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-        enter_button_match = cv2.matchTemplate(screen, enter_button_template, cv2.TM_CCOEFF_NORMED)
-        enter_button_location = np.where(enter_button_match >= MATCH_THRESHOLD)
-        if enter_button_location[0].size > 0:
-            # Click the Enter buttons
-            enter_button_x = 1670
-            enter_button_y = 780
-            win32api.SetCursorPos((enter_button_x, enter_button_y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, enter_button_x, enter_button_y, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, enter_button_x, enter_button_y, 0, 0)
+        screen_width1, screen_height1 = pyautogui.size()
+        search_height1, search_width1, _ = enter_raid_image.shape
+        search_range1 = (0, 0, screen_width1 - search_width1, screen_height1 - search_height1)
+
+        screenshot1 = np.array(pyautogui.screenshot())
+        gray_screenshot1 = cv2.cvtColor(screenshot1, cv2.COLOR_BGR2GRAY)
+        result1 = cv2.matchTemplate(gray_screenshot1, cv2.cvtColor(enter_raid_image, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
+        min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(result1)
+        
+        if max_val1 >= threshold:
+            print('Match found with a rate of:', max_val1)
+            win32gui.SetForegroundWindow(win32gui.FindWindow(None, "Diablo Immortal"))
+            win32api.SetCursorPos((200, 300))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 200, 300, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 200, 300, 0, 0)
+            print("Enter Raid button is clicked.")
+
             break
-        else:
-            print('Enter button not found.')
-            time.sleep(1)
+            
+        search_range1 = (max_loc1[0], max_loc1[1], search_range1[2], search_range1[3])
 
-    # Wait for the game to load
-    time.sleep(6)
+def clickEnterButton():
 
-    # Move the character to the boss
     while True:
-        rect = win32gui.GetWindowRect(hwnd)
-        x, y = rect[0], rect[1]
-        w, h = rect[2] - x, rect[3] - y
+        print('Enter Button is detecting...')
+        time.sleep(1)
 
-        screen = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-        ashava_text_match = cv2.matchTemplate(screen, ashava_text_template, cv2.TM_CCOEFF_NORMED)
-        ashava_text_location = np.where(ashava_text_match >= MATCH_THRESHOLD_ASHAVA)
-        if ashava_text_location[0].size > 0:
-            print('ASHAVA FOUND.')
-            DIKeys.press(hexKeyMap.DIK_W, 1.5)      
+        screen_width2, screen_height2 = pyautogui.size()
+        search_height2, search_width2, _ = enter_button_image.shape
+        search_range2 = (0, 0, screen_width2 - search_width2, screen_height2 - search_height2)
+
+        screenshot2 = np.array(pyautogui.screenshot())
+        gray_screenshot2 = cv2.cvtColor(screenshot2, cv2.COLOR_BGR2GRAY)
+        result2 = cv2.matchTemplate(gray_screenshot2, cv2.cvtColor(enter_button_image, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
+        min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(result2)
+        
+        if max_val2 >= threshold:
+            print('Match found with a rate of:', max_val2)
+            win32gui.SetForegroundWindow(win32gui.FindWindow(None, "Diablo Immortal"))
+            win32api.SetCursorPos((1750, 780))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 1750, 780, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 1750, 780, 0, 0)
+            print("Enter button is clicked.")
+
             break
+            
+        search_range2 = (max_loc2[0], max_loc2[1], search_range2[2], search_range2[3])
 
-        else:
-            if ashava_text_location[0].size > 0:
-                print('ASHAVA FOUND.')
-                DIKeys.press(hexKeyMap.DIK_W, 1.5) 
+def moveCharacterToAshava():
+
+    while True:
+        print('Moving character to Ashava...')
+        time.sleep(1)
+
+        screen_width3, screen_height3 = pyautogui.size()
+        search_height3, search_width3, _ = ashava_found_image.shape
+        search_range3 = (0, 0, screen_width3 - search_width3, screen_height3 - search_height3)
+
+        bossFound = True
+
+        while bossFound:
+            DIKeys.press(hexKeyMap.DIK_W) 
+            DIKeys.KeyDown(hexKeyMap.DIK_W) # Press key
+            screenshot3 = np.array(pyautogui.screenshot())
+            gray_screenshot3 = cv2.cvtColor(screenshot3, cv2.COLOR_BGR2GRAY)
+            result3 = cv2.matchTemplate(gray_screenshot3, cv2.cvtColor(ashava_found_image, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
+            min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result3)
+            
+            if max_val3 >= threshold:
+                print('Match found with a rate of:', max_val3)
+                print("Ashava is found.")
+                DIKeys.KeyUp(hexKeyMap.DIK_W) # Release key
+                DIKeys.press(hexKeyMap.DIK_W, 0.6) # Move a little towards the boss.
+                bossFound = False
                 break
+            search_range3 = (max_loc3[0], max_loc3[1], search_range3[2], search_range3[3])
+        break
 
-        DIKeys.press(hexKeyMap.DIK_W, 0.5)      
+def beginAttack():
 
-    # Spam the primary attack and use the other keys in the pre-defined intervals
     primary_attack_start_time = time.time()
     key1_last_time = 0
     key2_last_time = 0
@@ -120,78 +112,86 @@ while True:
     key4_last_time = 0
     key5_last_time = 0
 
+    time.sleep(4)
+    DIKeys.press(hexKeyMap.DIK_1, 0.2)
+    DIKeys.press(hexKeyMap.DIK_2, 0.2)
+    DIKeys.press(hexKeyMap.DIK_3, 0.2)
+    DIKeys.press(hexKeyMap.DIK_4, 0.2)
+
     while True:
-        rect = win32gui.GetWindowRect(hwnd)
-        x, y = rect[0], rect[1]
-        w, h = rect[2] - x, rect[3] - y
+        screen_width4, screen_height4 = pyautogui.size()
+        search_height4, search_width4, _ = ashava_found_image.shape
+        search_range4 = (0, 0, screen_width4 - search_width4, screen_height4 - search_height4)
+        screenshot4 = np.array(pyautogui.screenshot())
+        gray_screenshot4 = cv2.cvtColor(screenshot4, cv2.COLOR_BGR2GRAY)
+        result4 = cv2.matchTemplate(gray_screenshot4, cv2.cvtColor(ashava_found_image, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
+        min_val4, max_val4, min_loc4, max_loc4 = cv2.minMaxLoc(result4)
 
-        screen = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-        ashava_text_match = cv2.matchTemplate(screen, ashava_text_template, cv2.TM_CCOEFF_NORMED)
-        ashava_text_location = np.where(ashava_text_match >= MATCH_THRESHOLD_ASHAVA)
-        if ashava_text_location[0].size > 0:
-            # Spam the primary attack
+        if max_val4 >= threshold:
+            # Spam the primary attack and use skills in the pre-defined intervals
+            print('Attacking...')
+            print('Match found with a rate of:', max_val4)
+
             DIKeys.KeyDown(hexKeyMap.DIK_SPACE)
-
-            DIKeys.press(hexKeyMap.DIK_1, 0.2)
-            DIKeys.press(hexKeyMap.DIK_2, 0.2)
-            DIKeys.press(hexKeyMap.DIK_3, 0.2)
-            DIKeys.press(hexKeyMap.DIK_4, 0.2)
+            # DIKeys.press(hexKeyMap.DIK_1, 0.2)
+            # DIKeys.press(hexKeyMap.DIK_2, 0.2)
+            # DIKeys.press(hexKeyMap.DIK_3, 0.2)
+            # DIKeys.press(hexKeyMap.DIK_4, 0.2)
 
             # Check the intervals for Key 1 to 4
             time_elapsed = time.time() - primary_attack_start_time
             if time_elapsed - key1_last_time >= KEY1_INTERVAL:
                 DIKeys.press(hexKeyMap.DIK_1, 0.2)
                 key1_last_time = time_elapsed
+
             if time_elapsed - key2_last_time >= KEY2_INTERVAL:
                 DIKeys.press(hexKeyMap.DIK_2, 0.2)
                 key2_last_time = time_elapsed
+
             if time_elapsed - key3_last_time >= KEY3_INTERVAL:
                 DIKeys.press(hexKeyMap.DIK_3, 0.2)
                 key3_last_time = time_elapsed
+
             if time_elapsed - key4_last_time >= KEY4_INTERVAL:
                 DIKeys.press(hexKeyMap.DIK_4, 0.2) 
                 key4_last_time = time_elapsed
-            if time_elapsed - key5_last_time >= KEY4_INTERVAL:
+
+            if time_elapsed - key5_last_time >= KEY5_INTERVAL:
                 DIKeys.press(hexKeyMap.DIK_Q, 0.2) # Use the potion
                 key5_last_time = time_elapsed
-            
+                    
             DIKeys.KeyUp(hexKeyMap.DIK_SPACE)
 
-        else:
+        screen_width5, screen_height5 = pyautogui.size()
+        search_height5, search_width5, _ = ashava_dead_image.shape
+        search_range5 = (0, 0, screen_width5 - search_width5, screen_height5 - search_height5)
+        screenshot5 = np.array(pyautogui.screenshot())
+        gray_screenshot5 = cv2.cvtColor(screenshot5, cv2.COLOR_BGR2GRAY)
+        result5 = cv2.matchTemplate(gray_screenshot5, cv2.cvtColor(ashava_dead_image, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
+        min_val5, max_val5, min_loc5, max_loc5 = cv2.minMaxLoc(result5)
+
+        
+        if max_val5 >=  threshold:
+            print('Attacking is finished. Boss is dead.')
+            print('Match found with a rate of:', max_val5)
+
             time.sleep(2)
-            DIKeys.press(hexKeyMap.DIK_W, 2)
+            DIKeys.press(hexKeyMap.DIK_W, 1)
+            time.sleep(2)
+            DIKeys.press(hexKeyMap.DIK_E, 0.1)
+            time.sleep(2)
+            win32api.SetCursorPos((1150, 650))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 1150, 650, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 1150, 650, 0, 0)
+
             break
 
-    while True: 
-        time.sleep(1)
+while True:
+    if win32gui.FindWindow(None, "Diablo Immortal") == 0:
+        print('Error: Diablo Immortal window not found.')
 
-        rect = win32gui.GetWindowRect(hwnd)
-        x, y = rect[0], rect[1]
-        w, h = rect[2] - x, rect[3] - y
-
-        screen = np.array(pyautogui.screenshot(region=(x, y, w, h)))
-        exit_button_match = cv2.matchTemplate(screen, exit_raid_template, cv2.TM_CCOEFF_NORMED)
-        exit_button_location = np.where(exit_button_match >= MATCH_THRESHOLD_FIND_RAID)
-        if exit_button_location[0].size > 0:
-            # Click the Enter button
-            enter_button_x = 1570
-            enter_button_y = 140
-            win32gui.SetForegroundWindow(hwnd)
-            win32api.SetCursorPos((enter_button_x, enter_button_y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, enter_button_x, enter_button_y, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, enter_button_x, enter_button_y, 0, 0)
-
-            time.sleep(1.5)
-            enter_button_x = 1150
-            enter_button_y = 650
-            win32api.SetCursorPos((enter_button_x, enter_button_y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, enter_button_x, enter_button_y, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, enter_button_x, enter_button_y, 0, 0)
-            time.sleep(6)
-            break
-        else:
-            print('Raid not found.')
-            time.sleep(1)       
-    
-
-    
+    time.sleep(2)
+    clickEnterRaid()
+    clickEnterButton()
+    moveCharacterToAshava()
+    beginAttack()
